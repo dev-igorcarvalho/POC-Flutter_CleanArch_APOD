2d12bc9c727f61f65a58e618b88fe40841e23d7e
Igor Carvalho <igor.carvalho@datagrupo.com.br>
2022-06-15 07:57:11 -0300

    CHORE: Picture of day Creating a blank screen to be used later as the main screen Task: XXXX

--- lib/feature/picture_of_day/presentation/screen/nasa_pic_of_day.dart
+++ lib/feature/picture_of_day/presentation/screen/nasa_pic_of_day.dart
@@ -6,6 +6,12 @@ class NasaPicOfDayScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // var api = inject<PictureOfDayRepositoryImpl>();
+    // api
+    //     .fetchPicturesFromDateRange(
+    //         startDate: DateTime.now().subtract(Duration(days: 10)))
+    //     .then((value) => print(value));
+
     return Scaffold(
       body: Center(
         child: Text('NASA: pic o\'day'),
