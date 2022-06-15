723b1849fe3a135af99f3ecb881ce7723a75f36a
Igor Carvalho <igor.carvalho@datagrupo.com.br>
2022-06-15 07:42:59 -0300

    ARCH: dependency injection Implementing get it package as main Dependency Injection Task: XXXX

--- lib/core/locators/dependency_locator.dart
+++ lib/core/locators/dependency_locator.dart
@@ -1,12 +1,35 @@
+import 'package:daily_astronomy/core/config/config.dart';
+import 'package:daily_astronomy/feature/picture_of_day/data/datasource/picture_of_day_api.dart';
+import 'package:daily_astronomy/feature/picture_of_day/data/repository/picture_of_day_repository_impl.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart';
+
+import '../../feature/picture_of_day/data/datasource/picture_of_day_local_storage.dart';
 
 final GetIt inject = GetIt.instance;
 
-void initServiceLocator() {
+Future init() async {
+  await _initLocalStorage();
   _initCore();
   _initModules();
 }
 
+Future _initLocalStorage() async {
+  await Hive.initFlutter();
+  Box box = await Hive.openBox('APOD');
+  inject.registerSingleton(box);
+}
+
+void initConfig(Config config) {
+  inject.registerSingleton(config);
+}
+
 void _initCore() {}
 
-void _initModules() {}
+void _initModules() {
+  inject.registerFactory(() => PictureOfDayApi(Client()));
+  inject.registerFactory(() => PictureOfDayLocalStorage(inject<Box>()));
+  inject.registerFactory(() => PictureOfDayRepositoryImpl(
+      inject<PictureOfDayLocalStorage>(), inject<PictureOfDayApi>()));
+}
