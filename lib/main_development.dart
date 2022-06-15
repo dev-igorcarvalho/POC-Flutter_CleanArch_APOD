03873aa5c9f09b66dead25d7a83ffc29b5510156
Igor Carvalho <igor.carvalho@datagrupo.com.br>
2022-06-15 07:54:33 -0300

    ARCH: Application container initializer Creating a entry point with custom config for the app Task: XXXX

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,12 @@
 import 'package:daily_astronomy/core/application_container/daily_astronomy.dart';
+import 'package:daily_astronomy/core/config/config.dart';
+import 'package:daily_astronomy/core/locators/dependency_locator.dart';
 import 'package:flutter/widgets.dart';
 
-void main() {
+void main() async {
+  await init();
+  Config config = Config(Api('https://api.nasa.gov/planetary/apod',
+      'tB4U40zQLjlFoa6b26lTgbfDzhWzFfi8gaxe9pzI'));
+  initConfig(config);
   runApp(const DailyAstronomy());
 }
