2db5661f2df84f59744f868330b3a86fc6308f47
Igor Carvalho <igor.carvalho@datagrupo.com.br>
2022-06-18 15:23:17 -0300

    FEAT: Picture of Day Adding some over simplified UI Task: XXXX

--- lib/feature/picture_of_day/presentation/screen/nasa_pic_of_day.dart
+++ lib/feature/picture_of_day/presentation/screen/nasa_pic_of_day.dart
@@ -1,20 +1,36 @@
+import 'package:daily_astronomy/feature/picture_of_day/presentation/bloc/picture_of_day_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/locators/dependency_locator.dart';
 
 class NasaPicOfDayScreen extends StatelessWidget {
   const NasaPicOfDayScreen({Key? key}) : super(key: key);
   static const ROUTE_NAME = "/";
 
+  Widget mapStateToWidget(BuildContext context, PictureOfDayState state) {
+    if (state is PictureOfDayLoading) {
+      context.read<PictureOfDayBloc>().add(PictureOfDayFetchListEvent());
+      return CircularProgressIndicator();
+    } else if (state is PictureOfDayLoaded)
+      return Text(state.data.toString());
+    else if (state is PictureOfDayError)
+      return Text('Error');
+    else {
+      context.read<PictureOfDayBloc>().add(PictureOfDayLoadingEvent());
+      return Text('Empty');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // var api = inject<PictureOfDayRepositoryImpl>();
-    // api
-    //     .fetchPicturesFromDateRange(
-    //         startDate: DateTime.now().subtract(Duration(days: 10)))
-    //     .then((value) => print(value));
-
     return Scaffold(
-      body: Center(
-        child: Text('NASA: pic o\'day'),
+      body: BlocProvider(
+        create: (context) => inject<PictureOfDayBloc>(),
+        child: Center(
+            child: BlocBuilder<PictureOfDayBloc, PictureOfDayState>(
+          builder: (context, state) => mapStateToWidget(context, state),
+        )),
       ),
     );
   }
